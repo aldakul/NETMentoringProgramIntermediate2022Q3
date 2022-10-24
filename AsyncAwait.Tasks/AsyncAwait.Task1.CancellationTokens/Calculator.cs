d8d560999a4c63e2bdde761397f6902566296ec5
Bauyrzhan Aldakulov <Bauyrzhan_Aldakulov@epam.com>
2022-10-24 22:51:40 +0600

    push Task1

---
+++ AsyncAwait.Tasks/AsyncAwait.Task1.CancellationTokens/Calculator.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncAwait.Task1.CancellationTokens;
+
+internal static class Calculator
+{
+    public static async Task<long> Calculate(int n, CancellationToken token)
+    {
+        return await Task.Run((() => CalculateSum(n, token)));
+    }
+    private static long CalculateSum(int n, CancellationToken token)
+    {
+        long result = 0;
+        for (int i = 0; i < n; i++)
+        {
+            if (token.IsCancellationRequested)
+            {
+                return result;
+            }
+            result = result + (i + 1);
+            Thread.Sleep(10);
+        }
+        return result;
+    }
+}
