c51b6ced8c47289b107529a6ed3c0f66753e4d1c
Bauyrzhan Aldakulov <Bauyrzhan_Aldakulov@epam.com>
2022-11-21 21:34:13 +0600

    add Task 1

--- Expression/ExpressionTrees.Task1.ExpressionsTransformator/IncDecExpressionVisitor.cs
+++ Expression/ExpressionTrees.Task1.ExpressionsTransformator/IncDecExpressionVisitor.cs
@@ -1,9 +1,67 @@
-﻿using System.Linq.Expressions;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System;
+using System.Linq.Expressions;
 
 namespace ExpressionTrees.Task1.ExpressionsTransformer
 {
     public class IncDecExpressionVisitor : ExpressionVisitor
     {
-        // todo: feel free to add your code here
+        private Dictionary<string, double> _listOfValues = new Dictionary<string, double>();
+
+        protected override Expression VisitBinary(BinaryExpression node)
+        {
+            Expression expression;
+
+            if (node.Right is ConstantExpression c && Convert.ToInt32(c.Value) == 1)
+            {
+                if ((node.NodeType == ExpressionType.Add || node.NodeType == ExpressionType.Subtract) && 
+                    node.Left.NodeType == ExpressionType.Parameter && 
+                    !IsParameterReplaced((node.Left as ParameterExpression).Name))
+                {
+                    if (node.NodeType == ExpressionType.Add)
+                    {
+                        expression = Expression.Increment(node.Left);
+                    }
+                    else
+                    {
+                        expression = Expression.Decrement(node.Left);
+                    }
+                }
+                else
+                {
+                    expression = base.VisitBinary(node);
+                }
+            }
+            else
+            {
+                expression = base.VisitBinary(node);
+            }
+
+            return expression;
+        }
+
+        protected override Expression VisitLambda<T>(Expression<T> node)
+        {
+            return Expression.Lambda(typeof(T), Visit(node.Body), node.Parameters);
+        }
+
+        protected override Expression VisitParameter(ParameterExpression node)
+        {
+            return IsParameterReplaced(node.Name)
+                        ? Expression.Constant(_listOfValues[node.Name], node.Type)
+                        : base.VisitParameter(node);
+        }
+
+        private bool IsParameterReplaced(string parameterName)
+        {
+            return _listOfValues?.ContainsKey(parameterName) ?? false;
+        }
+
+        public Expression Execute(Expression expression, Dictionary<string, double> listOfValues = null)
+        {
+            _listOfValues = listOfValues;
+            return Visit(expression);
+        }
     }
 }
--- Expression/ExpressionTrees.Task1.ExpressionsTransformator/Program.cs
+++ Expression/ExpressionTrees.Task1.ExpressionsTransformator/Program.cs
@@ -7,18 +7,32 @@
  * The results could be printed in console or checked via Debugger using any Visualizer.
  */
 using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
 
 namespace ExpressionTrees.Task1.ExpressionsTransformer
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             Console.WriteLine("Expression Visitor for increment/decrement.");
             Console.WriteLine();
 
-            // todo: feel free to add your code here
+            //Example 1
+            Expression<Func<int, int>> inputExpression1 = i => (i + 1) + 2 * (i - 1);
+            Console.WriteLine($"Input exptession {inputExpression1}\n");
+            Expression outputExpression1 = new IncDecExpressionVisitor().Execute(inputExpression1);
+            Console.WriteLine($"Output exptession {outputExpression1}\n\n");
 
+            //Example 2
+            var listOfValues = new Dictionary<string, double>{{ "pi", Math.Round(Math.PI, 2)}};
+            Expression<Func<double, double, double, double, int, double>> inputExpression2 =
+                (pi, a, b, c, d) => Math.Pow(1.1, (a - 1) * (b - 1) / Math.Sqrt(1 + c) + pi) + Math.Round(1.2, d + 1);
+
+            Console.WriteLine($"Input exptession {inputExpression2}\n");
+            Expression outputExpression2 = new IncDecExpressionVisitor().Execute(inputExpression2, listOfValues);
+            Console.WriteLine($"Output exptession {outputExpression2} \n");
             Console.ReadLine();
         }
     }
